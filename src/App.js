169c5508941a45256edfce744bf1df04769b0c40
Yuri Martins <yurigamer16@gmail.com>
2019-12-13 01:55:50 -0200

    Create app react

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Meu primeiro app em React
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprenda mais em
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
