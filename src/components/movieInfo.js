6a688b6e34afc304813611d20933d05a1fcac068
Yuri Martins <yurigamer16@gmail.com>
2019-12-26 21:52:21 -0200

    Add class of movie

--- src/components/movieInfo.js
+++ src/components/movieInfo.js
@@ -13,6 +13,7 @@ const MovieInfo= ({ movieDetail}) => {
                 <section className="movie-detail text-center">
                   <img src={'https://image.tmdb.org/t/p/original' + movieDetail.poster_path} alt="imagem-filme" width="190" height="278" style={{borderRadius: 8, marginTop: 150}}></img>
                   <h5 className="mt-4 bold">{movieDetail.title}</h5>
+                  <div className="d-flex center m-auto bold" style={{width: 60, padding:12,backgroundColor: '#1e7e34', textAlign: 'center', color: '#fff',fontWeight: 'bold' }}>{movieDetail.adult === false ? 'Livre' : '+18'}</div>
                 </section>
             </div>
     </div>
